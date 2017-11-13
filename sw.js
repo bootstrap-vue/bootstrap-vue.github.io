9424e41c3bb240fe5781553ef1abff2f9ecac939
BootstrapVue <vuebootstrap@gmail.com>
2017-11-13 01:49:31 +0000

    Updates

--- sw.js
+++ sw.js
@@ -219,8 +219,8 @@ const fileManifest = [
     "revision": "22ed55c5b71328be765c0ca2a536599c"
   },
   {
-    "url": "/_nuxt/app.f7e9c0ae4c83e671107b.js",
-    "revision": "b2967fdd3d66e871c0cbf73f25554c49"
+    "url": "/_nuxt/app.adab66fa36d8da904d5b.js",
+    "revision": "69a33b371b014ede046456cc44007302"
   },
   {
     "url": "/_nuxt/common.3f47e97677a8eee89e8b25944bb08bcf.css",
@@ -239,8 +239,8 @@ const fileManifest = [
     "revision": "a625e0cd3dd11c485d146e5d879152f4"
   },
   {
-    "url": "/_nuxt/manifest.be277cb42d96f9971f6c.js",
-    "revision": "5cb6f9acd84509181334fadc3790a4c4"
+    "url": "/_nuxt/manifest.e02c3c38169744ea5975.js",
+    "revision": "961ca50496f3b73d9b5f165a18747ee7"
   },
   {
     "url": "/_nuxt/mSearch.6ff92b00a933f5e8f8f3.js",
